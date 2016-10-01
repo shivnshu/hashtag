77b31e9d934c53ba4d1a355535feb2f8578475b7
shivnshu <shivnshus@gmail.com>
2016-10-01 19:22:17 +0530

    made checkbox handler

--- src/com/example/hashtag/customcheckviewadapter.java
+++ src/com/example/hashtag/customcheckviewadapter.java
@@ -9,33 +9,74 @@ import android.widget.BaseAdapter;
 
 public class customcheckviewadapter extends BaseAdapter {
 
-	public customcheckviewadapter(Context context,List<String> t1,List<String> t2,List<String> t3,List<String> img ) {
+	List<String> result1;
+	List<String> result2;
+	List<String> result3;
+	List<Integer> img1;
+	CheckBox check;
+	LayoutInflater inflater=null;
+
+	public customlistviewadapter(Context context,List<String> t1,List<String> t2,List<String> t3,List<Integer> img ) {
 		// TODO Auto-generated constructor stub
-		
+		result1 = t1;
+		result2 = t2;
+		result3 = t3;
+		img1 = img;
+		inflater = ( LayoutInflater )context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
 
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return result1.size();
 	}
 
 	@Override
 	public Object getItem(int arg0) {
 		// TODO Auto-generated method stub
-		return null;
+		return arg0;
 	}
 
 	@Override
 	public long getItemId(int arg0) {
 		// TODO Auto-generated method stub
-		return 0;
+		return arg0;
+	}
+
+	public class Holder{
+		TextView t1;
+		TextView t2;
+		TextView t3;
+		ImageView img;
+		CheckBox check;
 	}
 
 	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
+	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		return null;
+		Holder holder = new Holder();
+		View rowView;
+		rowView = inflater.inflate(R.layout.customcheckview, null);
+		holder.t1 = (TextView)rowView.findViewById(R.id.ct1);
+		holder.t2 = (TextView)rowView.findViewById(R.id.ct2);
+		holder.t3 = (TextView)rowView.findViewById(R.id.ct3);
+		holder.img = (ImageView)rowView.findViewById(R.id.cimg);
+		holder.check = (CheckBox)rowView.findViewById(R.id.c);
+
+		//holder.t1.setText(result1[position]);
+		//holder.t2.setText(result2[position]);
+		//holder.t3.setText(result3[position]);
+
+		//holder.img.setImageResource(img1[position]);
+		
+		holder.t1.setText(result1.get(position));
+		holder.t2.setText(result2.get(position));
+		holder.t3.setText(result3.get(position));
+		holder.img.setImageResource(R.drawable.ic_launcher);
+		holder.check.checked=False;
+		
+		return rowView;
+
 	}
 
 }
